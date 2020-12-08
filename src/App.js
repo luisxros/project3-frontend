dc1b2acf0db985764435cd9f09a18b429500a0ef
Luis Rosario <luisalmonter@gmail.com>
2020-12-08 16:42:35 -0500

    adds route/switch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+
 import DashboardPage from './pages/DashboardPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+
+import { Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Switch>
+      <Route exact path="/"  render={ props =>
+        <HomePage />
+      }/>
+      <Route exact path="/dashboard"  render={ props =>
+        <DashboardPage />
+      }/>
+      <Route exact path="/login"  render={ props =>
+        <LoginPage />
+      }/>
+      <Route exact path="/signup"  render={ props =>
+        <SignupPage />
+      }/>
+      </Switch>
 
       <Footer />
     </div>
